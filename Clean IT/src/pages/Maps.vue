609c8dcda16bf30d2068755c7f5de18684a65c41
GILLET Florian <gilletf92@gmail.com>
2019-12-19 18:35:47 +0100

    ajout des points sur la carte

--- Clean IT/src/pages/Maps.vue
+++ Clean IT/src/pages/Maps.vue
@@ -3,30 +3,29 @@
     <div id="map" class="map"></div>
   </card>
 </template>
-	
+
 <script>
 import axios from "axios";
 
 export default {
   data() {
-      return {
-          objData : [],
-      }
+    return {
+      objData: [],
+      points: []
+    };
   },
   methods: {
     carteMesCouilles() {
       var map = L.map("map", { doubleClickZoom: false }).setView(
         [48.864716, 2.349014],
-        5
+        12
       );
 
       L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
         attribution: "OSM"
       }).addTo(map);
 
-      var points = [{ lat: 48.840542852103084, lng: 2.3212051391601562 }];
-
-      var heat = L.heatLayer(points, {
+      var heat = L.heatLayer(this.points, {
         maxZoom: 10
       }).addTo(map);
     }
@@ -34,17 +33,23 @@ export default {
   mounted() {
     this.carteMesCouilles();
   },
-  created() {
-      axios
-        .get(`http://localhost:3000/interventions/`)
-        .then(response => {
-          this.objData = response.data
-          console.log(this.objData)
-        })
-        .catch(e => {
-          console.log('ca passe pas bill')
-        })
-    },
-
+  beforeMount() {
+    axios
+      .get(`http://localhost:3000/interventions/`)
+      .then(response => {
+        this.objData = response.data;
+        console.log(this.objData);
+        for (var i = 0; i < this.objData.length; i++) {
+          this.points[i] = {
+            lat: this.objData[i].latitude,
+            lng: this.objData[i].longitude
+          };
+        }
+        console.log(this.points);
+      })
+      .catch(e => {
+        console.log("ca passe pas bill");
+      });
+  }
 };
 </script>
